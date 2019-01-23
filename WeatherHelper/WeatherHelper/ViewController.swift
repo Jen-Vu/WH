fa6f7c77bf941dd63e10654b6fe72acf9811d7e1
Vladyslav Zhulavskyi <vladyslavzhulavskyi@MacBook-Air-Vladyslav.local>
2019-01-23 18:35:13 +0200

    CreateProject

---
+++ WeatherHelper/WeatherHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherHelper
+//
+//  Created by Vladyslav Zhulavskyi on 1/23/19.
+//  Copyright © 2019 Vladyslav Zhulavskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
